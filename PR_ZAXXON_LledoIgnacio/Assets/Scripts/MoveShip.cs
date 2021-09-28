8e804c0840914744738470020385f133798c890a
Maik <ia4lledo@gmail.com>
2021-09-28 14:17:03 +0200

    Objetos y medio script de movimiento

---
+++ PR_ZAXXON_LledoIgnacio/Assets/Scripts/MoveShip.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveShip : MonoBehaviour
+{
+    private Vector3 initialPos = new Vector3 (1f,0f,0f);
+    private Vector3 leftMove;
+    private Vector3 rightMove;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
