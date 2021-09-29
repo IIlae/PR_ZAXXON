be19dee05ea55671a152d424e50e60df0ea264b5
Maik <ia4lledo@gmail.com>
2021-09-29 10:23:10 +0200

    Movimiento, rotación y límites
    movimiento y limites de mapa funcionando, rotación en progreso

--- PR_ZAXXON_LledoIgnacio/Assets/Scripts/MoveShip.cs
+++ PR_ZAXXON_LledoIgnacio/Assets/Scripts/MoveShip.cs
@@ -4,18 +4,50 @@ using UnityEngine;
 
 public class MoveShip : MonoBehaviour
 {
-    private Vector3 initialPos = new Vector3 (1f,0f,0f);
-    private Vector3 leftMove;
-    private Vector3 rightMove;
+    public int speed = 18;
+    private Vector3 initialPos = new Vector3 (0f,0f,0f);
+    private Vector3 initialRot = new Vector3 (0, 0, 0);
+    private Vector3 movePos = new Vector3 (0f, 0f, 0f);
+    private Vector3 moveRot = new Vector3 (0f, 0f, 0f);
+    private Vector3 rightMove = new Vector3 (1f, 0f, 0f);
+    private Vector3 leftMove = new Vector3 (-1f,0f,0f);
+    private Vector3 leftSpin = new Vector3 (0,0,40);
+    private Vector3 rightSpin = new Vector3 (0,0,-40);
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = initialPos;
+        transform.eulerAngles = initialRot;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.A))
+        {
+            movePos += leftMove * speed * Time.deltaTime;
+            transform.eulerAngles += leftSpin * Time.deltaTime;
+            if(movePos[0] >= -20)
+            {
+                transform.position = movePos;
+            }
+            else
+            {
+                movePos = transform.position;
+            }
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            movePos += rightMove * speed * Time.deltaTime;
+            transform.eulerAngles += rightSpin * Time.deltaTime;
+            if (movePos[0] <= 20)
+            {
+                transform.position = movePos;
+            }
+            else
+            {
+                movePos = transform.position;
+            }
+        }
     }
 }
